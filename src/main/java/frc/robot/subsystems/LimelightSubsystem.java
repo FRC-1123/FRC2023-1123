0804c69aa3e2772d1811ef28a6b4bb50eab67974
HanSolo626 <carsonjball@gmail.com>
2023-02-18 11:40:09 -0500

    something
    i don't remember

--- src/main/java/frc/robot/subsystems/LimelightSubsystem.java
+++ src/main/java/frc/robot/subsystems/LimelightSubsystem.java
@@ -4,18 +4,34 @@
 package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
 
 
-public class LimelightSubsystem {
+public class LimelightSubsystem extends SubsystemBase{
 
+    NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+    NetworkTableEntry tx = table.getEntry("tx");
+    NetworkTableEntry ty = table.getEntry("ty");
+    NetworkTableEntry ta = table.getEntry("ta");
     private void getLimelightData(){
-        double limelightData = NetworkTableInstance.getDefault().getTable("limelight").getEntry("<variablename>").getDouble(0);
-        System.out.println(limelightData);
+        double x = tx.getDouble(0.0);
+        double y = ty.getDouble(0.0);
+        double area = ta.getDouble(0.0);
+        System.out.println("X = "+x);
+        System.out.println("Y = "+y);
+        System.out.println("Area = "+area);
     }
-    
+
     public LimelightSubsystem(){
+    
+    }
+
+    public void periodic() {
+        System.out.println("hello");
         getLimelightData();
     }
     
