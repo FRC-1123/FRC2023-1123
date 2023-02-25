74590ebfd06cd0d02932d2f56d043543baeefbff
HanSolo626 <carsonjball@gmail.com>
2023-02-25 16:40:07 -0500

    can id update
    updated can ids and added code for lights

--- src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -27,28 +27,38 @@ AddressableLEDBuffer m_ledBuffer;
   
     }
 
+    public void initialize(){
+
+    }
+
     public void periodic(){
-        for (var i = 0; i < m_ledBuffer.getLength(); i++) {
-            // Sets the specified LED to the RGB values for red
-            m_ledBuffer.setRGB(i, 100, 100, 255);
-         }
+        // for (var i = 0; i < m_ledBuffer.getLength(); i++) {
+        //     // Sets the specified LED to the RGB values for red
+        //     m_ledBuffer.setRGB(i, 100, 100, 255);
+        //  }
          
-         m_led.setData(m_ledBuffer);
+        //  m_led.setData(m_ledBuffer);
+
+        // Fill the buffer with a rainbow
+        rainbow();
+        // Set the LEDs
+        m_led.setData(m_ledBuffer);
     }
 
     private void rainbow() {
-        // // For every pixel
-        // for (var i = 0; i < m_ledBuffer.getLength(); i++) {
-        //   // Calculate the hue - hue is easier for rainbows because the color
-        //   // shape is a circle so only one value needs to precess
-        //   final var hue = (m_rainbowFirstPixelHue + (i * 180 / m_ledBuffer.getLength())) % 180;
-        //   // Set the value
-        //   m_ledBuffer.setHSV(i, hue, 255, 128);
-        // }
-        // // Increase by to make the rainbow "move"
-        // m_rainbowFirstPixelHue += 3;
-        // // Check bounds
-        // m_rainbowFirstPixelHue %= 180;
+        // For every pixel
+        int m_rainbowFirstPixelHue = 0;
+        for (var i = 0; i < m_ledBuffer.getLength(); i++) {
+          // Calculate the hue - hue is easier for rainbows because the color
+          // shape is a circle so only one value needs to precess
+          final var hue = (m_rainbowFirstPixelHue + (i * 180 / m_ledBuffer.getLength())) % 180;
+          // Set the value
+          m_ledBuffer.setHSV(i, hue, 255, 128);
+        }
+        // Increase by to make the rainbow "move"
+        m_rainbowFirstPixelHue += 3;
+        // Check bounds
+        m_rainbowFirstPixelHue %= 180;
     
       }
 }
