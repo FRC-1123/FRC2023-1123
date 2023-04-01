176fdf2fef33830bd26b283b7dc077870023268e
HanSolo626 <carsonjball@gmail.com>
2023-04-01 17:02:01 -0400

    Added copilot controller
    the controller can control the intake and reset pose looking at driver

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -188,6 +188,7 @@ public final class Constants {
 
   public static final class OIConstants {
     public static final int kDriverControllerPort = 0;
+    public static final int kCopilotConrollerPort = 1;
   }
 
   public static final class AutoConstants {
